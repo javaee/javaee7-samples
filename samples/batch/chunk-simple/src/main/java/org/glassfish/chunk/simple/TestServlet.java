0ee3dc64a5e97eebe8b236b902a88ca3bc1885b4
Arun Gupta <arun.p.gupta@oracle.com>
2013-04-09 09:50:51 -0700

    Adding JobSecurityException - required per b14
    Former-commit-id: 29968a36273422b435b580f56c0d2ed5a666d3b1

--- samples/batch/chunk-simple/src/main/java/org/glassfish/chunk/simple/TestServlet.java
+++ samples/batch/chunk-simple/src/main/java/org/glassfish/chunk/simple/TestServlet.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.batch.operations.JobOperator;
+import javax.batch.operations.JobSecurityException;
 import javax.batch.operations.JobStartException;
 import javax.batch.runtime.BatchRuntime;
 import javax.batch.runtime.JobExecution;
@@ -62,7 +63,7 @@ public class TestServlet extends HttpServlet {
             }
             out.println("</body>");
             out.println("</html>");
-        } catch (JobStartException ex) {
+        } catch (JobStartException | JobSecurityException ex) {
             Logger.getLogger(TestServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
