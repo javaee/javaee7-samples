c9f578a1f29d6563afd6d41c790301e167e269df
Arun Gupta <arun.p.gupta@oracle.com>
2013-04-10 16:30:18 -0700

    Removing batch.xml, adding version attribute, adding @Named and beans.xml
    Former-commit-id: a40b760f3e61681ad545ef73bc21297ec44da091

--- samples/batch/multiple-steps/src/main/java/org/sample/multiple/steps/MyBatchlet.java
+++ samples/batch/multiple-steps/src/main/java/org/sample/multiple/steps/MyBatchlet.java
@@ -41,10 +41,12 @@
 package org.sample.multiple.steps;
 
 import javax.batch.api.AbstractBatchlet;
+import javax.inject.Named;
 
 /**
  * @author Arun Gupta
  */
+@Named
 public class MyBatchlet extends AbstractBatchlet {
 
     @Override
