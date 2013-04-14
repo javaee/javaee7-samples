96447da1b28dfbb47d0a00efba51eeaa9723a905
Arun Gupta <arun.p.gupta@oracle.com>
2013-04-13 21:39:32 -0700

    Adding a sout instead of UnsupportedOperationException
    Former-commit-id: e94e3f69228abe6573c06077465450c53618dea8

--- samples/concurrency/executor/src/main/java/org/glassfish/executor/MyWaitingTask.java
+++ samples/concurrency/executor/src/main/java/org/glassfish/executor/MyWaitingTask.java
@@ -46,7 +46,7 @@ public class MyWaitingTask implements Runnable {
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("MyWaitingTask.run");
     }
     
 }
