6b51938efbedc82ca936882d7dc73367bc08a1b5
Arun Gupta <arun.p.gupta@oracle.com>
2013-04-13 21:43:55 -0700

    Removing a redundant comment
    Former-commit-id: f7dc4642c4ae13c4c27ed0ed6a8d9e48235d0b32

--- samples/concurrency/executor/src/main/java/org/glassfish/executor/TestEJBServlet.java
+++ samples/concurrency/executor/src/main/java/org/glassfish/executor/TestEJBServlet.java
@@ -72,7 +72,6 @@ public class TestEJBServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
