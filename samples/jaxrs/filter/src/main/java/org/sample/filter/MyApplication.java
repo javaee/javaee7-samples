1e35b0616bf2cf3bd4b09d451c30ee09d968027c
Arun Gupta <arun.p.gupta@oracle.com>
2013-05-07 15:55:07 +0900

    Removing previously commented code
    Former-commit-id: b4cdece3e58811960b66a1ced02204c0fc17c484

--- samples/jaxrs/filter/src/main/java/org/sample/filter/MyApplication.java
+++ samples/jaxrs/filter/src/main/java/org/sample/filter/MyApplication.java
@@ -39,7 +39,6 @@
  */
 package org.sample.filter;
 
-import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -49,14 +48,4 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("webresources")
 public class MyApplication extends Application {
     
-    // Resource and filters need to be explicitly specified
-    // until http://java.net/jira/browse/JERSEY-1634 is fixed
-    
-//    @Override
-//    public Set<Class<?>> getClasses() {
-//        Set<Class<?>> resources = new java.util.HashSet<>();
-//        resources.add(org.sample.filter.MyResource.class);
-//        resources.add(org.sample.filter.ServerLoggingFilter.class);
-//        return resources;
-//    }
 }
