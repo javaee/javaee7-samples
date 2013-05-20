ddd3dcc0766da7dee2efb1b75aa7b8e7983e3707
Arun Gupta <arun.p.gupta@oracle.com>
2013-05-20 11:48:01 -0700

    Code clean up during rehearsals
    Former-commit-id: 2d9c2413de922188ca1f17f2f9173139e4c4dde9

--- launch-demos/jsonp/src/main/java/org/javaee7/samples/jsonp/FacebookObjectSearch.java
+++ launch-demos/jsonp/src/main/java/org/javaee7/samples/jsonp/FacebookObjectSearch.java
@@ -60,14 +60,14 @@ public class FacebookObjectSearch {
     public static void main(String... args) throws Exception {
         URL url = new URL("https://graph.facebook.com/search?q=java&type=post");
         try (InputStream is = url.openStream();
-                JsonReader rdr = Json.createReader(is)) {
+             JsonReader rdr = Json.createReader(is)) {
 
             JsonObject obj = rdr.readObject();
             JsonArray results = obj.getJsonArray("data");
             for (JsonObject result : results.getValuesAs(JsonObject.class)) {
                 System.out.print(result.getJsonObject("from").getString("name"));
                 System.out.print(": ");
-                System.out.println(result.getString("message"));
+                System.out.println(result.getString("message", ""));
                 System.out.println("-----------");
             }
         }
