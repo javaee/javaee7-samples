8a75223462a96b62ab6f9e216a9920d58c8850d5
Arun Gupta <arun.p.gupta@oracle.com>
2013-05-19 22:20:59 -0700

    Refactored package name
    Former-commit-id: ffdb09642d867c77a7c5f82ba3ea1437b7cd8de3

--- launch-demos/jsf/src/main/java/org/glassfish/samples/passthrough/UserBean.java
+++ launch-demos/jsf/src/main/java/org/glassfish/samples/passthrough/UserBean.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.glassfish.sample.passthrough;
+package org.glassfish.samples.passthrough;
 
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
