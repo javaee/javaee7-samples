c26cb722fc1218a2147f5fa747144c9efb0c27e8
Arun Gupta <arun.p.gupta@oracle.com>
2013-06-10 17:18:24 -0700

    Adding a new sample to monitor hashtags
    Former-commit-id: 5549d2dcafb154bc97ecc256cb4cd2e2a5ed161a

---
+++ samples/platform/twitter-hashtag/src/main/resources/META-INF/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TWEETS (
+    "from_user" VARCHAR(50) not null, 
+    "from_user_name" VARCHAR(50) not null, 
+    "from_user_id_str" VARCHAR(50) not null, 
+    "id" VARCHAR(50) not null, 
+    "text" VARCHAR(200) not null,
+    "created_at" VARCHAR(50) not null,
+    "profile_image_url" VARCHAR(50) not null
+)
