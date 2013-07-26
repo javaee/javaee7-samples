8cf3a7594270a6e700dccb858576291589e09223
Arun Gupta <arun.p.gupta@oracle.com>
2013-07-26 14:45:05 +0800

    Cleaning up chat server implementation, using session.getOpenSessions()
    Former-commit-id: fc6be2533222989a0601afe575df4bbba3287381

--- lab/solution/movieplex7/src/main/java/org/glassfish/movieplex7/chat/ChatServer.java
+++ lab/solution/movieplex7/src/main/java/org/glassfish/movieplex7/chat/ChatServer.java
@@ -40,13 +40,8 @@
 package org.glassfish.movieplex7.chat;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import javax.websocket.EncodeException;
-import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
@@ -56,23 +51,10 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/websocket")
 public class ChatServer {
 
-    private static final Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
-
-    @OnOpen
-    public void onOpen(Session peer) {
-        peers.add(peer);
-    }
-
-    @OnClose
-    public void onClose(Session peer) {
-        peers.remove(peer);
-    }
-
     @OnMessage
     public void message(String message, Session client) throws IOException, EncodeException {
-        for (Session peer : peers) {
-            if (!peer.equals(client))
-                peer.getBasicRemote().sendObject(message);
+        for (Session peer : client.getOpenSessions()) {
+            peer.getBasicRemote().sendObject(message);
         }
     }
 }
