faaefff29c5dcb2b4e08b5414bd11bdbcb8bb8bd
Arun Gupta <arun.p.gupta@oracle.com>
2013-07-30 15:46:35 -0700

    Using @OnMessage, @OnOpen instead of the simplified API
    Former-commit-id: c238456479b620d913aba0f84bfd4f02883752dc

--- lab/solution/movieplex7/src/main/java/org/glassfish/movieplex7/chat/ChatServer.java
+++ lab/solution/movieplex7/src/main/java/org/glassfish/movieplex7/chat/ChatServer.java
@@ -40,10 +40,16 @@
 package org.glassfish.movieplex7.chat;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
 
 /**
  * @author Arun Gupta
@@ -51,10 +57,22 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/websocket")
 public class ChatServer {
 
+private static final Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnOpen
+    public void onOpen(Session peer) {
+        peers.add(peer);
+    }
+
+    @OnClose
+    public void onClose(Session peer) {
+        peers.remove(peer);
+    }
+
     @OnMessage
     public void message(String message, Session client) throws IOException, EncodeException {
-        for (Session peer : client.getOpenSessions()) {
-            peer.getBasicRemote().sendObject(message);
+        for (Session peer : peers) {
+           peer.getBasicRemote().sendObject(message);
         }
     }
 }
