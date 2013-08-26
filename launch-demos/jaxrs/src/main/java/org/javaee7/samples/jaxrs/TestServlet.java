4500e54241765ec79f6cf62d3454acb8cc6fbdd9
Arun Gupta <arun.p.gupta@oracle.com>
2013-08-26 12:36:58 +0000

    Removing a redundant import
    Former-commit-id: c5326647362e260ba3eea947b174874bcb666af7

--- launch-demos/jaxrs/src/main/java/org/javaee7/samples/jaxrs/TestServlet.java
+++ launch-demos/jaxrs/src/main/java/org/javaee7/samples/jaxrs/TestServlet.java
@@ -50,7 +50,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedHashMap;
 
 /**
